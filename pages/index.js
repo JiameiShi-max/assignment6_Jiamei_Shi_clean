cf84ca729860b522794f518cdb0fffe158d6887b
apple <apple@appledeMacBook-Pro.local>
2026-04-18 20:53:55 +0800

    add index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Assignment 6</h1>
+      <p>
+        <a href="/assignment6">Go to Assignment 6</a>
+      </p>
+    </div>
+  );
+}
